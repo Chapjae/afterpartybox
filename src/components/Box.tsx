6483e43b225c3c49e05d73c38a04d6a5530c7553
Jay Liu <jayliu89@gmail.com>
2024-06-20 11:31:55 -0700

    adjusted camera and box model positions

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -7,9 +7,10 @@ import * as THREE from 'three';
 
 type BoxModelProps = {
   url: string;
+  position?: [number, number, number];
 };
 
-const BoxModel: React.FC<BoxModelProps> = ({ url }) => {
+const BoxModel: React.FC<BoxModelProps> = ({ url, position = [0, 0, 0] }) => {
   const gltf = useLoader(GLTFLoader, url);
   const modelRef = useRef<THREE.Object3D>();
 
@@ -20,14 +21,14 @@ const BoxModel: React.FC<BoxModelProps> = ({ url }) => {
   });
 
   return (
-    <group>
+    <group position={position}>
       <primitive ref={modelRef} object={gltf.scene} />
 
       <Text
-        fontSize={0.1}
+        fontSize={0.07}
         anchorX='center'
         color={'black'}
-        position={[0, -0.2, 0]}
+        position={[0, -0.1, 0]}
       >
         Only $10.99!
       </Text>
@@ -37,11 +38,11 @@ const BoxModel: React.FC<BoxModelProps> = ({ url }) => {
 
 const Box: React.FC<BoxModelProps> = ({ url }) => {
   return (
-    <Canvas camera={{ position: [0, 0, 0.2], near: 0.1 }}>
+    <Canvas camera={{ position: [0, 0, 0.3], near: 0.1 }}>
       <Suspense fallback={null}>
         <ambientLight intensity={1} />
         <pointLight position={[10, 10, 10]} />
-        <BoxModel url={url} />
+        <BoxModel url={url} position={[0, -0.04, -0.01]} />
         <OrbitControls minDistance={0.1} />
       </Suspense>
     </Canvas>
